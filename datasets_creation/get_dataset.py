176cf60e5f99a8dfff0fc075a49cac9693b2f06b
Phillweston <2436559745@qq.com>
2024-05-10 01:10:50 +0800

    Revise get_dataset script, add gitignore

--- datasets_creation/get_dataset.py
+++ datasets_creation/get_dataset.py
@@ -4,7 +4,9 @@ import wget
 import zipfile
 
 base_url = 'https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/'
-base_path = os.path.dirname('../../HDD_dataset/')
+script_dir = os.path.dirname(os.path.abspath(__file__))  # get the directory of the script
+base_path = os.path.join(script_dir, '../../HDD_dataset/')  # build the relative path from the script directory
+base_path = os.path.normpath(base_path)  # normalize the path (remove redundant separators and up-level references)
 years = ['2013', '2014', '2015', '2016', '2017', '2018', '2019']
 
 # zips contain different directory names or no directory at all, which causes
@@ -46,15 +48,29 @@ def main(years, base_path):
                     print("Downloading:", url)
                     wget.download(url, out=base_path)
                 print("\nUnzipping:", zip_path)
-                dest_path = year_path if unzip_dir is None else base_path
+                dest_path = os.path.join(base_path, unzip_dir) if unzip_dir else year_path
+                os.makedirs(dest_path, exist_ok=True)  # ensure dest_path exists
                 with zipfile.ZipFile(zip_path, 'r') as z:
                     z.extractall(dest_path)
 
                 if unzip_dir is not None and unzip_dir != y:
-                    unzip_path = os.path.join(dest_path, unzip_dir)
+                    unzip_path = dest_path
                     for f in os.listdir(unzip_path):
-                        shutil.move(os.path.join(unzip_path, f),
-                                os.path.join(year_path, f))
+                        print("Moving file:", f)
+                        src_file_path = os.path.join(unzip_path, f)
+                        dest_file_path = os.path.join(year_path, f)
+                        if os.path.isdir(src_file_path):
+                            # If f is a directory, move its contents to the destination directory
+                            for subf in os.listdir(src_file_path):
+                                dest_subdir = os.path.join(dest_file_path, subf)
+                                os.makedirs(os.path.dirname(dest_subdir), exist_ok=True)  # ensure the destination directory exists
+                                shutil.move(os.path.join(src_file_path, subf), dest_subdir)
+                        else:
+                            # If f is a file, move it to the destination directory
+                            os.makedirs(os.path.dirname(dest_file_path), exist_ok=True)  # ensure the destination directory exists
+                            shutil.move(src_file_path, dest_file_path)
+                        print("Moved file:", f, "to:", dest_file_path)
+                    print("Removing empty directory:", unzip_path)
                     os.rmdir(unzip_path)
 
 
