Fix multi-thread file processing, add new penalty calculation

algorithms/Classification.py
@@ -28,7 +28,7 @@ import logger
 
 # Define default global values
 TRAINING_PARAMS = {
-    'penalty': 1,
+    'reg': 0.1,
     'batch_size': 256,
     'lr': 0.001,
     'weight_decay': 0.01,
@@ -448,7 +448,7 @@ def classification(X_train, Y_train, X_test, Y_test, classifier, metric, **args)
             epochs=args['epochs'],                  # Total number of epochs
             batch_size=args['batch_size'],          # Batch size for training
             lr=args['lr'],                          # Learning rate
-            penalty=args['penalty'],                # Regularization penalty
+            reg=args['reg'],                        # Regularization parameter
             id_number=args['id_number']
         )
         # Run training and testing using the TCNTrainer
@@ -461,7 +461,7 @@ def classification(X_train, Y_train, X_test, Y_test, classifier, metric, **args)
             epochs=args['epochs'],                  # Total number of epochs
             batch_size=args['batch_size'],          # Batch size for training
             lr=args['lr'],                          # Learning rate
-            penalty=args['penalty'],                # Regularization penalty
+            reg=args['reg'],                        # Regularization parameter
             id_number=args['id_number']
         )
         # Run training and testing using the TCNTrainer
@@ -475,7 +475,7 @@ def classification(X_train, Y_train, X_test, Y_test, classifier, metric, **args)
             epochs=args['epochs'],                  # Total number of epochs
             batch_size=args['batch_size'],          # Batch size for training
             lr=args['lr'],                          # Learning rate
-            penalty=args['penalty'],                # Regularization penalty
+            reg=args['reg'],                        # Regularization parameter
             id_number=args['id_number']
         )
         # Run training and testing using the MLPTrainer
@@ -600,7 +600,7 @@ def set_training_params(*args):
     # Use the global keyword when modifying global variables
     global TRAINING_PARAMS
     TRAINING_PARAMS = {
-        'penalty': args['penalty'],
+        'reg': args['reg'],
         'batch_size': args['batch_size'],
         'lr': args['lr'],
         'weight_decay': args['weight_decay'],
@@ -722,7 +722,7 @@ def initialize_classification(*args):
         logger.info('Data imported successfully, processing smart attributes...')
         for column in list(df):
             missing = round(df[column].notna().sum() / df.shape[0] * 100, 2)
-            logger.info('{:.<27}{}%'.format(column, missing))
+            logger.info(f"{column:<27}.{missing}%")
         # Step 1.2: Filter out the bad HDDs.
         bad_missing_hds, bad_power_hds, df = filter_HDs_out(df, min_days=min_days_HDD, time_window='30D', tolerance=2)
         # predict_val represents the prediction value of the failure
@@ -739,7 +739,7 @@ def initialize_classification(*args):
             df = feature_selection(df, num_features)
         logger.info('Used features')
         for column in list(df):
-            logger.info('{:.<27}'.format(column,))
+            logger.info(f'{column:<27}.')
         logger.info(f'Saving to pickle file: {model}_Dataset_selected_windowed_{history_signal}_rank_{ranking}_{num_features}_overlap_{overlap}.pkl')
 
         output_dir = os.path.join(script_dir, '..', 'output')
@@ -787,11 +787,12 @@ def initialize_classification(*args):
     os.environ["CUDA_VISIBLE_DEVICES"] = CUDA_DEV
     if classifier == 'TCN':
         # Step 1.6.1: Set training parameters for TCN. Subflowchart: TCN Subflowchart.
-        penalty = TRAINING_PARAMS['penalty']
         batch_size = TRAINING_PARAMS['batch_size']
         lr = TRAINING_PARAMS['lr']
         weight_decay = TRAINING_PARAMS['weight_decay']  # L2 regularization parameter
         epochs = TRAINING_PARAMS['epochs']
+        optimizer_type = TRAINING_PARAMS['optimizer_type']
+        reg = TRAINING_PARAMS['reg']
         num_inputs = Xtrain.shape[1]
         # Calculate the data dimension based on the history signal and overlap.
         if windowing == 1:
@@ -817,18 +818,17 @@ def initialize_classification(*args):
 
         # Define the best parameters
         best_params = {
-            'penalty': penalty,
             'batch_size': batch_size,
             'lr': lr,
             'weight_decay': weight_decay,
             'epochs': epochs,
+            'reg': reg,
         }
 
         # Save the best parameters to a JSON file
         save_best_params_to_json(best_params, classifier, id_number)
     elif classifier == 'LSTM':
         # Step 1.6.2: Set training parameters for LSTM. Subflowchart: LSTM Subflowchart.
-        penalty = TRAINING_PARAMS['penalty']
         lr = TRAINING_PARAMS['lr']
         weight_decay = TRAINING_PARAMS['weight_decay']  # L2 regularization parameter
         batch_size = TRAINING_PARAMS['batch_size']
@@ -840,6 +840,7 @@ def initialize_classification(*args):
         # The dimensionality of the output space of the first fully connected layer
         fc1_hidden_s = TRAINING_PARAMS['fc1_hidden_s']
         optimizer_type = TRAINING_PARAMS['optimizer_type']
+        reg = TRAINING_PARAMS['reg']
         num_inputs = Xtrain.shape[1]
         net = FPLSTM(lstm_hidden_s, fc1_hidden_s, num_inputs, 2, dropout)
         if torch.cuda.is_available():
@@ -858,7 +859,6 @@ def initialize_classification(*args):
 
         # Define the best parameters
         best_params = {
-            'penalty': penalty,
             'batch_size': batch_size,
             'lr': lr,
             'weight_decay': weight_decay,
@@ -866,20 +866,21 @@ def initialize_classification(*args):
             'dropout': dropout,
             'lstm_hidden_s': lstm_hidden_s,
             'fc1_hidden_s': fc1_hidden_s,
+            'reg': reg,
         }
 
         # Save the best parameters to a JSON file
         save_best_params_to_json(best_params, classifier, id_number)
     elif classifier == 'MLP_Manual':
         # Step 1.6.4: Set training parameters for MLP. Subflowchart: MLP Subflowchart.
-        penalty = TRAINING_PARAMS['penalty']
         batch_size = TRAINING_PARAMS['batch_size']
         lr = TRAINING_PARAMS['lr']
         weight_decay = TRAINING_PARAMS['weight_decay']  # L2 regularization parameter
         epochs = TRAINING_PARAMS['epochs']
         input_dim = Xtrain.shape[1] * Xtrain.shape[2]  # Number of features in the input
         hidden_dim = TRAINING_PARAMS['hidden_dim']  # Example hidden dimension, can be adjusted
-
+        optimizer_type = TRAINING_PARAMS['optimizer_type']
+        reg = TRAINING_PARAMS['reg']
         logger.info(f'number of inputs: {input_dim}, hidden_dim: {hidden_dim}')
         net = MLP(input_dim=input_dim, hidden_dim=hidden_dim)
         if torch.cuda.is_available():
@@ -898,12 +899,12 @@ def initialize_classification(*args):
 
         # Define the best parameters
         best_params = {
-            'penalty': penalty,
             'batch_size': batch_size,
             'lr': lr,
             'weight_decay': weight_decay,
             'epochs': epochs,
             'hidden_dim': hidden_dim,
+            'reg': reg,
         }
 
         # Save the best parameters to a JSON file
@@ -948,7 +949,7 @@ def initialize_classification(*args):
             epochs=epochs,
             batch_size=batch_size,
             lr=lr,
-            penalty=penalty,
+            reg=reg,
             id_number=id_number
         )
     except:

algorithms/Dataset_manipulation.py
@@ -271,10 +271,9 @@ def process_file(f, progress_list, args, name, all_data, total_files, model):
     
     with lock:
         progress_list.value += 1
-
-    all_data.append(data)
-    progress_percentage = (progress_list.value / total_files) * 100
-    print(f'Progress: {progress_percentage:.2f}%', end="\r")
+        all_data.append(data)
+        progress_percentage = (progress_list.value / total_files) * 100
+        print(f'Progress: {progress_percentage:.2f}%', end="\r")
 
     return data
 
@@ -312,7 +311,8 @@ def import_data(years, model, name, **args):
             progress_list = manager.Value('i', 0)
 
             for y in years:
-                logger.info('\nProcessing year:', y)
+                print('\n')
+                logger.info(f'Processing year: {y}')
                 dir_path = os.path.join(script_dir, '..', 'HDD_dataset', y)
                 if not os.path.exists(dir_path):
                     logger.error(f"Error: Directory {dir_path} does not exist.")
@@ -775,7 +775,7 @@ class DatasetPartitioner:
                 X = self.arrays_to_matrix(X, data_dim)
             else:
                 X = np.expand_dims(X, axis=1)  # Add an extra dimension
-            logger.info('Augmented data of predict_val is: ', Counter(y))
+            logger.info(f'Augmented data of predict_val is: {Counter(y)}')
             # Print the shapes of the train and test sets
             # Xtrain: ndarray, Xtest: ndarray, ytrain: Series, ytest: Series
             Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, stratify=y, test_size=self.test_train_perc, random_state=42)
@@ -1046,7 +1046,7 @@ def feature_selection(df, num_features):
     features = []
     dict1 = {}
 
-    logger.info('Number of feature selected for classification:', num_features)
+    logger.info(f'Number of feature selected for classification: {num_features}')
 
     # Step 1.4.2: For each feature in df.columns
     for feature in df.columns:

algorithms/Networks_pytorch.py
@@ -11,6 +11,7 @@ from datetime import datetime
 import os
 import logger
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.autograd import grad
 
 
 # these 2 functions are used to rightly convert the dataset for LSTM prediction
@@ -300,7 +301,7 @@ def report_metrics(Y_test_real, prediction, metric, writer, iteration):
     return f1_score(Y_test_real, prediction)
 
 class LSTMTrainer:
-    def __init__(self, model, optimizer, epochs, batch_size, lr, penalty, id_number):
+    def __init__(self, model, optimizer, epochs, batch_size, lr, reg, id_number):
         """
         Initialize the LSTMModelTrainer with all necessary components.
 
@@ -310,6 +311,7 @@ class LSTMTrainer:
             epochs (int): Number of training epochs.
             batch_size (int): Batch size for training.
             lr (float): Learning rate for the optimizer.
+            reg (float): Regularization factor.
             id_number (int): The ID number of the model.
         """
         self.model = model
@@ -317,7 +319,7 @@ class LSTMTrainer:
         self.epochs = epochs
         self.batch_size = batch_size
         self.lr = lr
-        self.penalty = penalty
+        self.reg = reg
         self.id_number = id_number
         self.train_writer = SummaryWriter('runs/LSTM_Training_Graph')
         self.scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10)
@@ -337,6 +339,23 @@ class LSTMTrainer:
         x_batch = torch.stack(xx).permute(1, 0, 2)
         y_batch = torch.stack(yy)
         return (x_batch, y_batch)
+
+    # Function to calculate the total loss combining error and penalty terms
+    def calculate_total_loss(self, error, reg):
+        """
+        Calculates the total loss for the model.
+
+        Parameters:
+        - error: The error term representing the model's prediction error.
+        - reg: The regularization parameter.
+
+        Returns:
+        - total_loss: The total loss, which is a combination of the prediction error and regularization penalty.
+        """
+        grads = grad(error, self.model.parameters(), create_graph=True)
+        penalty = sum(g.pow(2).mean() for g in grads)
+        total_loss = reg * error + (1 - reg) * penalty
+        return total_loss
 
     def train(self, Xtrain, ytrain, epoch):
         """
@@ -368,7 +387,9 @@ class LSTMTrainer:
             output = self.model(sequences)  # Forward pass through the model
             output_softmax = F.softmax(output, dim=1)  # Apply softmax to the output
             loss = criterion(output, labels)  # Calculate loss between model output and true labels
-            loss.backward()  # Backward pass to calculate gradients
+            # Calculate the total loss (error + penalty)
+            total_loss = self.calculate_total_loss(loss, self.reg)
+            total_loss.backward()  # Backward pass to calculate gradients
             self.optimizer.step()  # Update model parameters
             # Store the predicted labels for this batch in the predictions array
             predictions[(batch_idx * batchsize):((batch_idx + 1) * batchsize), :] = output_softmax.cpu().detach().numpy()
@@ -509,7 +530,7 @@ class LSTMTrainer:
         return model_path
 
 class TCNTrainer:
-    def __init__(self, model, optimizer, epochs, batch_size, lr, penalty, id_number):
+    def __init__(self, model, optimizer, epochs, batch_size, lr, reg, id_number):
         """
         Initialize the TCNTrainer with all necessary components for training and testing.
 
@@ -519,6 +540,7 @@ class TCNTrainer:
             epochs (int): Number of training epochs.
             batch_size (int): Batch size for training.
             lr (float): Initial learning rate.
+            reg (float): Regularization factor.
             id_number (int): The ID number of the model.
         """
         self.model = model
@@ -526,11 +548,28 @@ class TCNTrainer:
         self.epochs = epochs
         self.batch_size = batch_size
         self.lr = lr
-        self.penalty = penalty
+        self.reg = reg
         self.id_number = id_number
         self.train_writer = SummaryWriter('runs/TCN_Training_Graph')
         self.scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10)
         self.test_writer = SummaryWriter('runs/TCN_Test_Graph')
+
+    # Function to calculate the total loss combining error and penalty terms
+    def calculate_total_loss(self, error, reg):
+        """
+        Calculates the total loss for the model.
+
+        Parameters:
+        - error: The error term representing the model's prediction error.
+        - reg: The regularization parameter.
+
+        Returns:
+        - total_loss: The total loss, which is a combination of the prediction error and regularization penalty.
+        """
+        grads = grad(error, self.model.parameters(), create_graph=True)
+        penalty = sum(g.pow(2).mean() for g in grads)
+        total_loss = reg * error + (1 - reg) * penalty
+        return total_loss
 
     def train(self, Xtrain, ytrain, epoch):
         """
@@ -576,8 +615,10 @@ class TCNTrainer:
             output = self.model(data)
             # Calculate the loss between the predictions and the target
             loss = criterion(output, target.long())
+            # Calculate the total loss (error + penalty)
+            total_loss = self.calculate_total_loss(loss, self.reg)
             # Perform backpropagation to calculate the gradients of the loss with respect to the model parameters
-            loss.backward()
+            total_loss.backward()
             # Update the model parameters using the gradients and the optimizer
             self.optimizer.step()
             # Update the learning rate using the scheduler
@@ -720,7 +761,7 @@ class TCNTrainer:
         logger.info('Model saved as:', model_path)
 
 class MLPTrainer:
-    def __init__(self, model, optimizer, epochs, batch_size, lr, penalty, id_number):
+    def __init__(self, model, optimizer, epochs, batch_size, lr, reg, id_number):
         """
         Initialize the MLPTrainer with all necessary components for training and testing.
 
@@ -730,6 +771,7 @@ class MLPTrainer:
             epochs (int): Number of training epochs.
             batch_size (int): Batch size for training.
             lr (float): Initial learning rate.
+            reg (float): Regularization factor.
             id_number (int): The ID number of the model.
         """
         self.model = model
@@ -737,11 +779,27 @@ class MLPTrainer:
         self.epochs = epochs
         self.batch_size = batch_size
         self.lr = lr
-        self.penalty = penalty
+        self.reg = reg
         self.id_number = id_number
         self.train_writer = SummaryWriter('runs/MLP_Training_Graph')
         self.scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10)
         self.test_writer = SummaryWriter('runs/MLP_Test_Graph')
+
+    def calculate_total_loss(self, error, reg):
+        """
+        Calculates the total loss for the model.
+
+        Parameters:
+        - error: The error term representing the model's prediction error.
+        - reg: The regularization parameter.
+
+        Returns:
+        - total_loss: The total loss, which is a combination of the prediction error and regularization penalty.
+        """
+        grads = grad(error, self.model.parameters(), create_graph=True)
+        penalty = sum(g.pow(2).mean() for g in grads)
+        total_loss = reg * error + (1 - reg) * penalty
+        return total_loss
 
     def train(self, Xtrain, ytrain, epoch):
         """
@@ -779,7 +837,9 @@ class MLPTrainer:
             output = self.model(data)
             output_softmax = F.softmax(output, dim=1)  # Apply softmax to the output
             loss = criterion(output, target.long())
-            loss.backward()
+            # Calculate the total loss (error + penalty)
+            total_loss = self.calculate_total_loss(loss, self.reg)
+            total_loss.backward()  # Backward pass to calculate gradients
             self.optimizer.step()
             self.scheduler.step(loss)   # Update the learning rate using the scheduler
             predictions[(batch_idx * self.batch_size):((batch_idx + 1) * self.batch_size), :] = output_softmax.cpu().detach().numpy()